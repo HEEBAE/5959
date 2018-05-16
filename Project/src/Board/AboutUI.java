3077ede03d545cf05caece58729c2030d2578e1a
heebae <heebae@DESKTOP-V3PQ5C9>
2018-05-16 16:28:19 +0900

    AboutUI 05160628

--- Project/src/Board/AboutUI.java
+++ Project/src/Board/AboutUI.java
@@ -15,45 +15,41 @@ import java.awt.Font;
 public class AboutUI extends JFrame implements ActionListener {
 	public AboutUI() {
 		getContentPane().setBackground(Color.WHITE);
-		setSize(450, 450);
+		setSize(450, 468);
 		getContentPane().setLayout(null);
 
 		JLabel logo = new JLabel("");
 		logo.setHorizontalAlignment(SwingConstants.CENTER);
 		logo.setIcon(new ImageIcon("logo.png"));
-		logo.setBounds(0, 0, 434, 200);
+		logo.setBounds(0, 12, 434, 200);
 		getContentPane().add(logo);
 
-		JLabel lblNewLabel = new JLabel("\uAC1C\uBC1C\uC790");
+		JLabel lblNewLabel = new JLabel("<html>Project Gaebalgoebal Goebalgaebal <br><br>Team Oguogung <br><br> Chief Programmer<br> JU JAEMIN <br><br>Programmer & DBA <br>& Designer<br> JEON HEEBAE </html>");
 		lblNewLabel.setFont(new Font("³ª´®°íµñÄÚµù", Font.PLAIN, 13));
-		lblNewLabel.setBounds(50, 250, 57, 15);
+		lblNewLabel.setBounds(41, 146, 207, 255);
 		getContentPane().add(lblNewLabel);
-
-		JLabel lblNewLabel_1 = new JLabel("\uC804\uD76C\uBC30");
-		lblNewLabel_1.setFont(new Font("³ª´®°íµñÄÚµù", Font.PLAIN, 13));
-		lblNewLabel_1.setBounds(65, 280, 57, 15);
-		getContentPane().add(lblNewLabel_1);
-
-		JLabel lblNewLabel_2 = new JLabel("\uC8FC\uC7AC\uBBFC");
-		lblNewLabel_2.setFont(new Font("³ª´®°íµñÄÚµù", Font.PLAIN, 13));
-		lblNewLabel_2.setBounds(65, 310, 57, 15);
-		getContentPane().add(lblNewLabel_2);
 
 		JLabel lblVersion = new JLabel("Version 1.0.0");
 		lblVersion.setFont(new Font("³ª´®°íµñÄÚµù", Font.PLAIN, 13));
-		lblVersion.setBounds(50, 180, 141, 40);
+		lblVersion.setBounds(325, 12, 93, 40);
 		getContentPane().add(lblVersion);
 
-		JLabel lblThanksTo = new JLabel("Thanks to");
+		JLabel lblThanksTo = new JLabel("<html>Thanks to <br>JSS Sensei <br> Class C no minna</html>");
 		lblThanksTo.setFont(new Font("³ª´®°íµñÄÚµù", Font.PLAIN, 13));
-		lblThanksTo.setBounds(65, 340, 70, 15);
+		lblThanksTo.setBounds(175, 307, 130, 67);
 		getContentPane().add(lblThanksTo);
-
-		JButton btnNewButton = new JButton("\uD655\uC778");
-		btnNewButton.setFont(new Font("³ª´®°íµñÄÚµù", Font.PLAIN, 13));
-		btnNewButton.setBounds(151, 371, 110, 30);
-		btnNewButton.addActionListener(this);
-		getContentPane().add(btnNewButton);
+		
+		JLabel lblcCopyright = new JLabel("<html>(c)Copyright Oguogung's Engineers,<br> Programmers and others , 2018.  <br>All rights reserved.</html>");
+		lblcCopyright.setBounds(175, 210, 253, 71);
+		getContentPane().add(lblcCopyright);
+		
+		JLabel lblAllUse = new JLabel("New label");
+		lblAllUse.setIcon(new ImageIcon("C:\\Users\\heebae\\Desktop\\ProjectBackup\\\uC804\uCCB4\uC774\uC6A9\uAC00.PNG"));
+		lblAllUse.setBounds(20, 10, 50, 58);
+		getContentPane().add(lblAllUse);
+		
+		 
+		
 		
 		setTitle("About");
 
